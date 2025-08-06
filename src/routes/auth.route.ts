ae39393f4cf81d81434ebc15d7177a15c0717537
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-08-06 21:00:36 +0530

    feat:added /google-login endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
 import { LoginController } from "../controller/auth.controller";
 const route = Router()
-route.get("/google-login", LoginController)
+route.post("/google-login", LoginController)
 export default route;
