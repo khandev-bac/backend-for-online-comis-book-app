64984f803347a250c620959bed74f2acbae115a8
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-08-06 20:49:42 +0530

    feat:added commom /auth route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import { envField } from "./config/env"
+import authRouter from "./routes/auth.route"
 const app = express()
 app.use(express.json())
 app.get("/", (req: Request, res: Response) => {
@@ -7,6 +8,7 @@ app.get("/", (req: Request, res: Response) => {
         message: "hello"
     })
 })
+app.use("/auth", authRouter)
 app.listen(envField.PORT, () => {
     console.log(`Server is running on port ${envField.PORT}`)
 })
