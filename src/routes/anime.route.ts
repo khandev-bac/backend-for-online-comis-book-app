3d4bf4885094f66c532049c99f9fbc4e268157e7
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-08-06 21:15:57 +0530

    feat:added anime routes

--- src/routes/anime.route.ts
+++ src/routes/anime.route.ts
@@ -1,3 +1,11 @@
 import { Router } from "express"
+import { createAnimeController, deleteAnimeController, getAllAnimeController, getAnimeByIdController, updateAnimeController } from "../controller/anime.controller";
+import { AuthMiddleware } from "../middleware/AuthMiddlware";
 const router = Router()
+router.use(AuthMiddleware)
+router.post("/", createAnimeController)
+router.get("/", getAllAnimeController)
+router.get("/:animeId", getAnimeByIdController)
+router.post("/:animeId", updateAnimeController)
+router.delete("/:animeId", deleteAnimeController)
 export default router;
