cda7c2f30415403fa5057662319133e07cfa43f0
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-08-06 23:29:12 +0530

    feat:small update

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,2 +1,7 @@
 import { PrismaClient } from "../generated/prisma";
 export const prisma = new PrismaClient()
+// async function main() {
+//     const users = await prisma.user.findMany();
+//     console.log(users);
+// }
+// main()
