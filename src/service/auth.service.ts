9e0de5b2e7baa4da33f776dcf837511943ef99bd
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-08-06 11:22:16 +0530

    added Login service or authservice

---
+++ src/service/auth.service.ts
@@ -0,0 +1,41 @@
+import { verifyGoogleToken } from "../libs/google_login";
+import { generateToken } from "../libs/token";
+import { createUserRepo } from "../repo/auth.repo";
+import { findByEmail } from "../repo/user.repo";
+import { CreateUser } from "../types/user/createUserType";
+
+export const Login = async (idToken: string) => {
+    try {
+        const googleUser = await verifyGoogleToken(idToken)
+        if (!googleUser || !googleUser.email) {
+            return {
+                message: "Something went wrong.try again later"
+            }
+        }
+        let user = await findByEmail(googleUser.email)
+        if (!user) {
+            const newUser: CreateUser = {
+                userName: googleUser.userName!,
+                email: googleUser.email,
+                profileImage: googleUser.profileImage
+            }
+            user = await createUserRepo(newUser)
+        }
+        const tokens = generateToken({
+            username: user.userName,
+            email: user.email,
+            id: user.id
+        })
+        return {
+            message: "Successfully account created",
+            accessToken: tokens.accessToken,
+            refreshToken: tokens.refreshToken
+        }
+    } catch (error: any) {
+        console.error("AUTH_SERVICE_ERROR:", error.message);
+        return {
+            message: "failed to create account.try again later"
+        }
+        throw error;
+    }
+}
